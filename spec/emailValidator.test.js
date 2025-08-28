c03d9a4fd4dc644b34fcd70bf7fbe6672003d602
mathilde <mathilde.masualavodrama@student.vinci.be>
2025-08-28 17:14:58 +0200

    refactor : document

--- spec/emailValidator.test.js
+++ spec/emailValidator.test.js
@@ -1,40 +1,40 @@
 import { Main } from "../src/emailValidator.js";
 
 const main = new Main();
-describe("Email Validator", function() {
+describe("Email Validator", function () {
 
-    it("IsValid return false when given test.com", function() {
-      let email = "test.com";
-      expect(main.IsValid(email)).toBe(false);
-    });
-
-    it("IsValid return false when given mathy@vinci.com", function() {
-      let email = "mathy@vinci.com";
-      expect(main.IsValid(email)).toBe(true);
-    });
-
-    it("IsValid return false when given mathy@vinci  .com", function() {
-      let email = "mathy@vinci .com";
-      expect(main.IsValid(email)).toBe(false);
-    });
-
-    it("IsValid return false when given mathy@", function() {
-      let email = "mathy@";
-      expect(main.IsValid(email)).toBe(false);
-    });
-
-    it("isValid return false when  given @student.vinci.be", function() {
-      let email = "@student.vinci.be";
-      expect(main.IsValid(email)).toBe(false);
-    });
-
-    it("isValid return true when given mathy@student.vinci.com", function() {
-      let email = "mathy@student.vinci.com";
-      expect(main.IsValid(email)).toBe(true);
-    }); 
-
-    it("isValid return false when given mathy@studentvinci", function() {
-      let email = "mathy@studentvinci";
-      expect(main.IsValid(email)).toBe(false);
-    });
+  it("IsValid return false when given test.com", function () {
+    let email = "test.com";
+    expect(main.IsValid(email)).toBe(false);
   });
+
+  it("IsValid return false when given mathy@vinci.com", function () {
+    let email = "mathy@vinci.com";
+    expect(main.IsValid(email)).toBe(true);
+  });
+
+  it("IsValid return false when given mathy@vinci  .com", function () {
+    let email = "mathy@vinci .com";
+    expect(main.IsValid(email)).toBe(false);
+  });
+
+  it("IsValid return false when given mathy@", function () {
+    let email = "mathy@";
+    expect(main.IsValid(email)).toBe(false);
+  });
+
+  it("isValid return false when  given @student.vinci.be", function () {
+    let email = "@student.vinci.be";
+    expect(main.IsValid(email)).toBe(false);
+  });
+
+  it("isValid return true when given mathy@student.vinci.com", function () {
+    let email = "mathy@student.vinci.com";
+    expect(main.IsValid(email)).toBe(true);
+  });
+
+  it("isValid return false when given mathy@studentvinci", function () {
+    let email = "mathy@studentvinci";
+    expect(main.IsValid(email)).toBe(false);
+  });
+});
--- src/emailValidator.js
+++ src/emailValidator.js
@@ -1,35 +1,35 @@
 export class Main {
 
 
-      IsValid(email){
+    IsValid(email) {
         return this.checkAtFormat(email) && this.containsPeriod(email) && this.containtsSpace(email);
     }
-    containsAt(email){
-      
+    containsAt(email) {
+
         return /@/.test(email);
     }
 
-    containsPeriod(email){
-      
-        if(email.endsWith(".")){
+    containsPeriod(email) {
+
+        if (email.endsWith(".")) {
             return false;
         }
-        
+
         return (/\./).test(email);
     }
 
-    containtsSpace(email){
-        if(email.includes(" ")){
+    containtsSpace(email) {
+        if (email.includes(" ")) {
             return false;
         }
         return true;
     }
 
-    checkAtFormat(email){
+    checkAtFormat(email) {
 
-           return this.containsAt(email) && email.indexOf('@') < email.length - 1 &&  email.indexOf('@') > 0 ;
+        return this.containsAt(email) && email.indexOf('@') < email.length - 1 && email.indexOf('@') > 0;
     }
 
-  
+
 }
 export default Main;
