d04997139e1ba98aca2683a2839e98062de558ba
mathilde <mathilde.masualavodrama@student.vinci.be>
2025-08-28 16:26:55 +0200

    add : containsAt return false when given test.com

--- spec/emailValidator.test.js
+++ spec/emailValidator.test.js
@@ -4,4 +4,12 @@ describe("Main", () => {
   it("should be defined", () => {
     expect(Main).toBeDefined();
   });
+
+  describe("containsAt check if email containt @",function(){
+    it("containsAt return false when given test.com", function() {
+      let email = "test.com";
+      let main = new Main();
+      expect(main.containsAt(email)).toBe(false);
+    });
+  });
 });
--- src/emailValidator.js
+++ src/emailValidator.js
@@ -1,4 +1,10 @@
 export class Main {
 
+    containsAt(email){
+        if(/@/.test(email)){
+            return true;
+        }
+        return false;
+    }
 }
 export default Main;
